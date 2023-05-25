77ff312a7203d6b671f9bf266f9f5a37577f79ba
adityapermono8 <adityapermono8@gmail.com>
2023-05-25 12:16:29 +0700

    mengatur alur interaksi dengan pengguna melalui menu.

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -162,6 +162,22 @@ void DoubleLinkedList::searchData() {
 		cout << "\nName: " << curr->name << endl;
 	}
 }
-int main() {
 
+int main() {
+	DoubleLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all record in the ascending order of roll numbers" << endl;
+			cout << "4. View all record in the descending order of roll numbers" << endl;
+			cout << "5. Search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter tour choice (1-6): ";
+			char ch;
+			cin >> ch;
+
+		}
+	}
 }
