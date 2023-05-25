5a097a54b8222ec0cffb97fdd13255f532bb46b8
adityapermono8 <adityapermono8@gmail.com>
2023-05-25 11:00:23 +0700

    menambahkan node baru dan memastikan urutan yang benar berdasarkan nomor roll.

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -42,4 +42,19 @@ void DoubleLinkedList::addNode() {
 	newNode->noMhs = nim;
 	newNode->name = nm;
 
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number no allowed" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;	
+	}
+	Node* current = START;
+	while (current->next != NULL && nim > current->next->noMhs)
+		current = current->next;
 }
