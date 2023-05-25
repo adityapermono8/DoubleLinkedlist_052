e00b56f8fd219c0bb59e1d94e996016e9e250456
adityapermono8 <adityapermono8@gmail.com>
2023-05-25 11:58:48 +0700

    mengisi metod searchData() pada kelas DoubleLinkedList

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -144,3 +144,8 @@ void DoubleLinkedList::hapus() {
 	else
 		cout << "Record with roll number " << rollNo << " deleted" << endl;
 }
+void DoubleLinkedList::searchData() {
+	if (listEmpty() == true) {
+
+	}
+}
