c92308b2bac70aec971c42001487ed62925c8f6e
adityapermono8 <adityapermono8@gmail.com>
2023-05-25 12:09:17 +0700

    Mencari data dalam linked list berdasarkan nomor roll yang diberikan.

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -146,6 +146,18 @@ void DoubleLinkedList::hapus() {
 }
 void DoubleLinkedList::searchData() {
 	if (listEmpty() == true) {
-
+		cout << "\nList is empty" << endl;
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search: ";
+	int num;
+	cin >> num;
+	if (DoubleLinkedList::search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else {
+		cout << "\nRecord found" << endl;
+		cout << "\nRecord number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
 	}
 }
